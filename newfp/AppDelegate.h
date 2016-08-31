a277be9bae3360860634ffc3f3f7fb82ecba2e54
Austin Vershel <avershel@gmail.com>
2016-08-31 10:19:21 -0400

    Initial Commit

---
+++ newfp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newfp
+//
+//  Created by Austin Vershel on 8/31/16.
+//  Copyright © 2016 Austin Vershel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newfp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  newfp
+//
+//  Created by Austin Vershel on 8/31/16.
+//  Copyright © 2016 Austin Vershel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
